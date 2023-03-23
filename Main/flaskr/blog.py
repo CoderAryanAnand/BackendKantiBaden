d99a19c28b65edff9680780652befe46e5702dde
SirCraft007 <seramol@gmx.ch>
2023-03-23 17:22:53 +0100

    just for fun

--- Main/flaskr/blog.py
+++ Main/flaskr/blog.py
@@ -1,13 +1,12 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
+from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
-
-
 @bp.route('/')
 def index():
     db = get_db()
@@ -17,7 +16,6 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
-
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
