c6bb4228a21c89a628662a93916ee719862e9188
Komet07 <simonlouis@bluewin.ch>
2023-03-29 09:46:37 +0200

    Making a *functional* Create game function

--- Main/flaskr/blog.py
+++ Main/flaskr/blog.py
@@ -24,10 +24,10 @@ def index(game):
     return render_template("Games/" + game + ".html", games=games)
 
 
-@bp.route("/<game>/create", methods=("GET", "POST"))
+@bp.route("/Games/create", methods=("GET", "POST"))
 @login_required
 def create(game):
-    if request.method == "POST":
+    if request.method == "post":
         uname = request.form["uname"]
         link = request.form["link"]
         code = request.form["code"]
