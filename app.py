168c16827a5a9bbcd8b9b304755f01236b62e2ca
CoderAryanAnand <68732506+CoderAryanAnand@users.noreply.github.com>
2023-05-16 16:40:24 +0200

    temp

--- app.py
+++ app.py
@@ -26,38 +26,18 @@ def index():
     else:
         return render_template('index.html', message="Hello!")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@app.route("/G1", methods=["GET", "POST"])
-def game_1():
-    username = session["username"]
-    print(username, file=sys.stderr)
-    return render_template("G1.html", username)
-
-<<<<<<< HEAD
-=======
->>>>>>> parent of 6c350bc (game database, game implementation)
-
-=======
->>>>>>> parent of 5e23ebb (games can now be implemented)
-=======
 
 @app.route("/Games/<game>", methods=("GET", "POST"))
 def game_1(game):
     return render_template("Games/" + game + ".html")
 
 
->>>>>>> 5e23ebbd2b1ce952ccb70ebfca13fea11ade2bac
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         try:
-<<<<<<< HEAD
-            db.session.add(User(username=request.form['username'], password=generate_password_hash(request.form['password'])))
-=======
             db.session.add(
                 User(username=request.form['username'], password=generate_password_hash(request.form['password'])))
->>>>>>> 5e23ebbd2b1ce952ccb70ebfca13fea11ade2bac
             db.session.commit()
             return redirect(url_for('login'))
         except:
@@ -74,15 +54,7 @@ def login():
         u = request.form['username']
         p = request.form['password']
         data = User.query.filter_by(username=u).first()
-<<<<<<< HEAD
-<<<<<<< HEAD
-        print(data, sys.stderr)
-=======
         print(data)
->>>>>>> parent of 6c350bc (game database, game implementation)
-=======
-        print(data)
->>>>>>> 5e23ebbd2b1ce952ccb70ebfca13fea11ade2bac
         if data is not None:
             if check_password_hash(data.password, p):
                 session['logged_in'] = True
@@ -95,15 +67,8 @@ def logout():
     session['logged_in'] = False
     return redirect(url_for('index'))
 
-<<<<<<< HEAD
-if __name__ == '__main__':
-    app.secret_key = "ThisIsNotASecret:p"
-    db.create_all()
-    app.run()
-=======
 
 if __name__ == '__main__':
     app.secret_key = "ThisIsNotASecret:p"
     db.create_all()
     app.run(debug=True)
->>>>>>> 5e23ebbd2b1ce952ccb70ebfca13fea11ade2bac
