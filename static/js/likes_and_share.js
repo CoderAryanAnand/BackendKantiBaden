7f924de0480e545cc3532f85105ab43a2ec578f7
CoderAryanAnand <68732506+CoderAryanAnand@users.noreply.github.com>
2023-05-19 16:04:27 +0200

    create functioning share button

--- static/js/likes_and_share.js
+++ static/js/likes_and_share.js
@@ -14,3 +14,18 @@ function like(gameId) {
         })
         .catch((e) => alert("Could not like game."));
 }
+
+$('.copy_text').click(function (e) {
+   e.preventDefault();
+   var copyText = $(this).attr('href');
+   var temp = new URL(copyText, document.baseURI).href
+   copyText = temp
+
+   document.addEventListener('copy', function(e) {
+      e.clipboardData.setData('text/plain', copyText);
+      e.preventDefault();
+   }, true);
+
+   document.execCommand('copy');
+   alert("Copied text!");
+ });
